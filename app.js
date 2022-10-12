bc67a006979fad44d248395e6e4eaf835bd06c99
fmoncef <moncef.fahssi@gmail.com>
2022-10-12 08:31:53 +0200

    saveWed

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-let objetGlobal = {name:"nameTest" , address:{address1:"testAdresse"} , informations:["testProductionLigne1" , "testUnitNumberProdution1" , "testLigneNumber1"]};
+let objetGlobal = {
+    name:"marcoussy" , 
+    address:{address1:"testAdresse"} , 
+    productionLines: [
+        {id:1, productionRate:1000},
+        {id:2, productionRate:500},
+                
+    ]
+};
 
 const express = require('express')
 const app = express()
@@ -17,10 +25,21 @@ app.get('/site-info', (req, res) => {
     res.json(objetGlobal)
 })
 
-app.post('/post-example', (req, res) => {
+app.post('/new-production-line', (req, res) => {
+let newId = objetGlobal.productionLines.length + 1;
+    objetGlobal.productionLines.push({id:newId, productionRate:0});
 
-    res.send("data received")
+
+    res.send("new-production-line received")
 })
+
+app.post('/production-line/update', (req, res) => {
+    console.log(req);
+        // objetGlobal.productionLines.update({id = req.id, productionRate = req.update});
+    
+    
+        res.send("production-line updated")
+    })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
